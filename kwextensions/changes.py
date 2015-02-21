3ff6303472076caca69238d40727dbeac4c49d4c
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-02-21 13:09:52 -0500

    Make mergerequest comment point back to the buildbot link.

--- kwextensions/changes.py
+++ kwextensions/changes.py
@@ -208,7 +208,9 @@ class GitlabMergeRequestPoller(base.PollingChangeSource, StateMixin):
 
     def _accept_change(self, mr):
         self.api.createmergerequestewallnote(mr["project_id"], mr["id"],
-                "BUILDBOT: Will schedule builds soon")
+                "**BUILDBOT**:Will schedule builds soon." \
+                "You can monitor the status [here](http://hera:8010/grid?%s)" % \
+                        urllib.urlencode({'branch': mr['source_branch']}))
 
     def _reject_change(self, mr):
         #self.api.createmergerequestewallnote(mr["project_id"], mr["id"],
