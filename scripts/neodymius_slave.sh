f85248abea9f377f4f086694a7de4c78e580ce64
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-02-24 11:45:02 -0500

    Update neodymius environment.

--- scripts/neodymius_slave.sh
+++ scripts/neodymius_slave.sh
@@ -5,5 +5,12 @@
 # since we're using mesa, no need to do offscreen screenshots.
 export DISPLAY=:0
 
+# Load intel compiler environment.
+source /opt/intel/bin/compilervars.sh intel64
+
+# Load modules needed for ParaView.
+source /usr/share/Modules/init/bash
+module load mpi/impi-x86_64
+
 source /home/kitware/Dashboards/buildbot-sandbox/bin/activate
 buildslave start /home/kitware/Dashboards/buildbot-slave
