680a31da7e1a22884a9d36791163e1672e8a4d18
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-02-24 12:00:25 -0500

    Added builders for neodymius.

--- scripts/neodymius.py
+++ scripts/neodymius.py
@@ -5,9 +5,46 @@ from buildbot.process.properties import Property, Interpolate
 
 slave = BuildSlave('neodymius', 'XXXXXXXX',
         max_builds=1,
-        properties = {}
+        properties = {
+            'cmakeroot': "/usr",
+            'sharedresourcesroot': "/home/kitware/Dashboards",
+
+            'os' : 'linux',
+            'distribution' : 'Fedora?',
+            'compiler' : 'icc',
+            "generator" : "Makefiles",
+
+            # Add site-specific cmake options here.
+            'configure_options:buildslave' : {
+                "CMAKE_CXX_FLAGS:STRING"  : "-Wall -Wextra -Wno-shadow -Wno-unused-function -Woverloaded-virtual -Wno-deprecated",
+                "CMAKE_C_FLAGS:STRING" : "-Wall -Wextra -Wno-shadow -Wno-unused-function",
+                "MPIEXEC:FILEPATH" : "mpiexec.hydra",
+                "PARAVIEW_BUILD_CATALYST_ADAPTORS:BOOL" : "ON"
+
+                "BUILD_EXAMPLES:BOOL" : "ON",
+                "BUILD_SHARED_LIBS:BOOL" : "ON",
+                'CMAKE_BUILD_TYPE:STRING' : 'Release',
+                "PARAVIEW_ENABLE_PYTHON:BOOL" : "ON",
+                "PARAVIEW_USE_MPI:BOOL" : "ON",
+                "VTK_DEBUG_LEAKS:BOOL"  : "ON",
+                },
+            'test_excludes:buildslave' : [
+                #  ProbePicking -- pick fails
+                "ProbePicking",
+                # TestPythonView -- no matplotlib
+                "TestPythonView"
+                ]
         )
 
+builders = {}
+builders["ParaView"] = [
+    BuilderConfig(name="linux-icc-shared-python-mpi-release",
+        slavenames=["neodymius"],
+        factory=factory.get_ctest_buildfactory(),
+        properties = {},
+        env = {'CC' : 'icc', 'CXX' : 'icpc'}
+        )
+    ]
 
 def get_buildslave():
     """Returns the BuildSlave instance for this machine"""
@@ -15,4 +52,7 @@ def get_buildslave():
 
 def get_builders(project="ParaView"):
     """Returns a list of build configurations for this slave for a specific project."""
-    return []
+    try:
+        return builders[project]
+    except KeyError:
+        return []
