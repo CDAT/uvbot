5ed98446cc4d341b465bfcb3b6d0c8fcd52656b7
Ben Boeckel <ben.boeckel@kitware.com>
2015-02-24 13:03:17 -0500

    paraview: add configuration info for paraview

---
+++ projects/paraview/__init__.py
@@ -0,0 +1,54 @@
+__all__ = [
+    'OPTIONS',
+    'OPTIONORDER',
+
+    'FEATURES',
+]
+
+OPTIONS = {
+    'libtype': {
+        'shared': {
+            'BUILD_SHARED_LIBS:BOOL': 'ON',
+        },
+        'static': {
+            'BUILD_SHARED_LIBS:BOOL': 'OFF',
+        },
+    },
+    'buildtype': {
+        'release': {
+            'CMAKE_BUILD_TYPE:STRING': 'Release',
+        },
+        'debug': {
+            'CMAKE_BUILD_TYPE:STRING': 'Debug',
+        },
+    },
+}
+OPTIONORDER = ('libtype', 'buildtype')
+
+FEATURES = {
+    'python': {
+        'PARAVIEW_ENABLE_PYTHON:BOOL': ('OFF', 'ON'),
+        'VTK_WRAP_PYTHON:BOOL': ('OFF', 'ON'),
+    },
+    'tcl': {
+        'VTK_WRAP_TCL:BOOL': ('OFF', 'ON'),
+    },
+    'java': {
+        'VTK_WRAP_JAVA:BOOL': ('OFF', 'ON'),
+    },
+    'kits': {
+        'VTK_ENABLE_KITS:BOOL': ('OFF', 'ON'),
+    },
+    'gui': {
+        'PARAVIEW_BUILD_QT_GUI:BOOL': ('OFF', 'ON'),
+    },
+    'mpi': {
+        'PARAVIEW_USE_MPI:BOOL': ('OFF', 'ON'),
+    },
+    'qt5': {
+        'PARAVIEW_QT_VERSION:STRING': ('5', '4'),
+    },
+    'unified': {
+        'PARAVIEW_USE_UNIFIED_BINDINGS:BOOL': ('OFF', 'ON'),
+    },
+}
