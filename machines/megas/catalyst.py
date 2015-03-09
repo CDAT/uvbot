d9bbba36b449dabb16769968c925dcaf27b68c33
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-03-09 12:36:26 -0400

    Update catalyst builders.
    We no longer make separate builders for each edition.

--- machines/megas/catalyst.py
+++ machines/megas/catalyst.py
@@ -16,39 +16,10 @@ env = {
 buildsets = [
     {
         'os': 'linux',
-        'editionset' : 'Base',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Essentials',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Essentials+Extras',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Essentials+Extras+Rendering-Base',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Enable-Python',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Enable-Python+Essentials',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Enable-Python+Essentials+Extras',
-    },
-    {
-        'os': 'linux',
-        'editionset' : 'Base+Enable-Python+Essentials+Extras+Rendering-Base+Rendering-Base-Python',
     },
 ]
 
-BUILDERS = catalyst.make_builders(catalyst, buildsets,
+BUILDERS = projects.make_builders(catalyst, buildsets,
     defprops=defprops,
     defconfig=defconfig,
     slavenames=['megas'],
