a98348faadb085a3265655b1bac69a070c6b96b1
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-03-09 16:21:49 -0400

    Use 64bit CMake on Miranda.
    This address the issue with upload files.

--- machines/miranda/slave.py
+++ machines/miranda/slave.py
@@ -9,7 +9,7 @@ __all__ = [
 SLAVE = BuildSlave('miranda', secrets.SECRETS['miranda']['password'],
     max_builds=1,
     properties={
-        'cmakeroot': 'C:/Tools/cmake-3.1.3',
+        'cmakeroot': 'C:/Tools/cmake-3.1.3-x64',
         'sharedresourcesroot': 'C:/Dashboards/MyTests',
 
         'os': 'windows',
