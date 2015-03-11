ce88ab58a5ee7419def4345845cd20db7ab02779
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-11 12:52:58 -0400

    nemesis: remove vcvarsall from nemesis.bat

--- machines/nemesis/start.bat
+++ machines/nemesis/start.bat
@@ -1,5 +1,3 @@
-call "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/vcvarsall.bat" x64
-
 set dashboardroot=%USERPROFILE%/dashboards/buildbot
 set buildbotroot=%USERPROFILE%/misc/root/buildbot
 
