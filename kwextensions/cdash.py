709acfce4c3545239e07499a0f7127ba030a5770
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-13 12:53:30 -0400

    cdash: cosmetics

--- kwextensions/cdash.py
+++ kwextensions/cdash.py
@@ -1,4 +1,3 @@
-
 from urllib import urlencode
 from datetime import datetime, timedelta
 
@@ -17,7 +16,7 @@ class DateOp(object):
     IS_BEFORE = 84
 
 class Query(object):
-    def __init__(self, project, filters=[], filtercombine="and"):
+    def __init__(self, project, filters=[], filtercombine='and'):
         self.project = project
         self.showfilters = 0
         self.limit = 100
@@ -25,9 +24,9 @@ class Query(object):
         self.__filtercombine = filtercombine
         self.__filters = filters
 
-    def add_filter(self, filter, op="and"):
+    def add_filter(self, filter, op='and'):
         if len(self.__filters) != 0 and self.__filtercombine != op:
-            raise RuntimeError, "Incompatible filter combination specified (%s != %s)" % (self.__filtercombine, op)
+            raise RuntimeError, 'Incompatible filter combination specified (%s != %s)' % (self.__filtercombine, op)
         self.__filtercombine = op
         self.__filters.append(filter)
 
@@ -42,7 +41,7 @@ class Query(object):
             idx = i + 1
             field, compare, value = self.__filters[i]
             if isinstance(value, datetime):
-                value = value.strftime("%Y%m%d")
+                value = value.strftime('%Y%m%d')
             q = { 'field%d' % idx : field,
                   'compare%d' % idx : compare,
                   'value%d' % idx : value
@@ -51,6 +50,6 @@ class Query(object):
         return query
 
     def get_url(self, root):
-        return root + "?" + urlencode(self.get_query_dict())
+        return root + '?' + urlencode(self.get_query_dict())
 
-# print Query("ParaView", filters=[("buildname/string", StringOp.CONTAINS, "superbuild")]).get_url("https://open.cdash.org/index.php")
+# print Query('ParaView', filters=[('buildname/string', StringOp.CONTAINS, 'superbuild')]).get_url('https://open.cdash.org/index.php')
