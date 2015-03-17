6f6a3bac5e8f91cc287a31e79bc4ff2a87920f03
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-17 11:12:06 -0400

    debiandash: add a superbuilds on the Debian VMs

--- machines/__init__.py
+++ machines/__init__.py
@@ -1,6 +1,8 @@
 from . import amber8
 from . import blight
 from . import dashlin1
+from . import debian6dash
+from . import debian7dash
 from . import kamino
 from . import megas
 from . import nemesis
@@ -17,6 +19,8 @@ MACHINES = [
     amber8,
     blight,
     dashlin1,
+    debian6dash,
+    debian7dash,
     kamino,
     megas,
     nemesis,
---
+++ machines/debian7dash/slave.py
@@ -0,0 +1,28 @@
+from buildbot.buildslave import BuildSlave
+
+from machines import secrets
+
+__all__ = [
+    'SLAVE',
+]
+
+SLAVE = BuildSlave('debian6dash', secrets.SECRETS['debian6dash']['password'],
+    max_builds=1,
+    properties={
+        #'cmakeroot': '/home/kitware/misc/root/cmake', # 3.1.3
+        'cmakeroot': '/usr', # 2.8.9
+        'sharedresourcesroot': '/home/kitware/dashboards',
+
+        'os': 'linux',
+        'distribution': 'debian-7-x86_64',
+        'compiler': 'gcc-4.7.2',
+
+        'generator': 'Unix Makefiles',
+        'buildflags': '-j3',
+
+        'configure_options:buildslave': {
+            'CMAKE_CXX_FLAGS:STRING': '-Wall -Wextra -Wshadow -Woverloaded-virtual -Wno-deprecated',
+            'CMAKE_C_FLAGS:STRING': '-Wall -Wextra -Wshadow',
+        },
+    }
+)
