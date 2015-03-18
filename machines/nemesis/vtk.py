74062dd6fb2b4bd6bc486773f61047575c5b7bef
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-18 14:11:19 -0400

    nemesis: set examples to be off explicitly

--- machines/nemesis/vtk.py
+++ machines/nemesis/vtk.py
@@ -10,7 +10,7 @@ defprops = {}
 
 defconfig = {
     # Examples end up with commands that are way too long.
-    #'BUILD_EXAMPLES:BOOL': 'ON',
+    'BUILD_EXAMPLES:BOOL': 'OFF',
     'BUILD_TESTING:BOOL': 'ON',
     'VTK_DEBUG_LEAKS:BOOL': 'ON',
     'VTK_LEGACY_REMOVE:BOOL': 'ON',
