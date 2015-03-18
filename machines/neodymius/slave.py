9ea7df11dc19461fc231b9ebadd485163294a4f7
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-18 16:05:11 -0400

    neodymius: port over nightly vtk dashboards

--- machines/neodymius/slave.py
+++ machines/neodymius/slave.py
@@ -14,7 +14,6 @@ SLAVE = BuildSlave('neodymius', secrets.SECRETS['neodymius']['password'],
 
         'os': 'linux',
         'distribution': 'fedora-19-x86_64',
-        'compiler': 'icc-14.0.0',
 
         'generator': 'Unix Makefiles',
         'buildflags': '-j9',
---
+++ machines/neodymius/vtk.py
@@ -0,0 +1,69 @@
+import projects
+from projects import vtk
+from . import slave
+
+__all__ = [
+    'BUILDERS',
+]
+
+defprops = {
+    'compiler': 'icc-14.0.0',
+}
+env = {
+    'DISPLAY': ':0',
+    'CC': 'icc',
+    'CXX': 'icpc',
+}
+
+defconfig = {
+    'BUILD_EXAMPLES:BOOL': 'ON',
+    'BUILD_TESTING:BOOL': 'ON',
+    'VTK_DEBUG_LEAKS:BOOL': 'ON',
+
+    'VTK_DATA_STORE:PATH': '/home/kitware/Dashboards/ExternalData/vtk',
+}
+
+allconfig = projects.merge_config(defconfig, {
+    'VTK_BUILD_ALL_MODULES:BOOL': 'ON',
+    'VTK_BUILD_ALL_MODULES_FOR_TESTS:BOOL': 'ON',
+})
+
+buildsets = [
+    {
+        'os': 'linux',
+        'libtype': 'shared',
+        'buildtype': 'release',
+        'features': (
+            'python',
+            'tcl',
+            'java',
+
+            'icc',
+        ),
+    },
+]
+
+BUILDERS = projects.make_builders(slave.SLAVE, vtk, buildsets,
+    defprops=defprops,
+    defconfig=allconfig,
+    env=env
+)
+
+glnewprops = projects.merge_config(defprops, {
+    'compiler': 'gcc-4.8.3',
+    'ctest_track' : 'VolumeOpenGLNew',
+})
+glnewenv = projects.merge_config(env, {
+    'CC': 'gcc',
+    'CXX': 'g++',
+})
+
+glnewconfig = projects.merge_config(defconfig, {
+    'Moddule_vtkRenderingVolumeOpenGLNew:BOOL': 'ON',
+})
+
+BUILDERS += projects.make_builders(slave.SLAVE, vtk, glnewbuildsets,
+    defprops=glnewprops,
+    defconfig=glnewconfig,
+    env=glnewenv
+)
