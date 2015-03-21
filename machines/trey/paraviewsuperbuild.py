9669130b6a7521c1aed0704201381a6e41d9c5f9
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-03-21 08:22:22 -0400

    Fix missing sysroot/target etc.

--- machines/trey/paraviewsuperbuild.py
+++ machines/trey/paraviewsuperbuild.py
@@ -35,6 +35,16 @@ defconfig = {
     'ENABLE_vistrails:BOOL': 'ON',
 
     'download_location:PATH': '/Users/kitware/dashboards/paraview-superbuild-downloads',
+
+    # Set OsX SYSROOT, etc.
+    'CMAKE_OSX_ARCHITECTURES:STRING' : 'x86_64',
+    'CMAKE_OSX_DEPLOYMENT_TARGET:STRING'  : '10.10',
+    'CMAKE_OSX_SYSROOT:PATH' : '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk',
+
+    # Pick Python version.
+    'PYTHON_EXECUTABLE:FILEPATH' : '/usr/bin/python2.7',
+    'PYTHON_INCLUDE_DIR:PATH' : '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/Python.framework/Versions/2.7/Headers',
+    'PYTHON_LIBRARY:FILEPATH' : '/usr/lib/libpython2.7.dylib',
 }
 
 buildsets = [
