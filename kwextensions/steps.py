9e0b290686c2704240ba391f038bf972c2db3f2d
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-03-23 23:23:17 -0400

    Trying more basic approach this time!

--- kwextensions/steps.py
+++ kwextensions/steps.py
@@ -59,7 +59,7 @@ def failureForSubmodule(step):
     from buildbot.status.builder import FAILURE
     lastStep = step.build.getStatus().getSteps()[0]
     output = lastStep.getLogs()[0].getText()
-    return step.build.result == FAILURE and output.find('in submodule path') != -1
+    return lastStep.build.result == FAILURE and output.find('in submodule path') != -1
 
 def makeUploadFetchSubmoduleScript(**kwargs):
     import os
