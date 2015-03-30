ab789b78a980f1362c85a5426bfc1f933a62f15b
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-30 16:15:46 -0400

    machines: add a local machine
    Really only useful for developers, so hide it behind an environment
    variable.

--- machines/__init__.py
+++ machines/__init__.py
@@ -33,3 +33,9 @@ MACHINES = [
     trey,
     miranda
 ]
+
+import os
+
+if 'KW_BUILDBOT_TESTING' in os.environ:
+    from . import local
+    MACHINES = [local]
---
+++ machines/local/__init__.py
@@ -0,0 +1,45 @@
+r'''
+    Machine: local
+    Owner: you!
+'''
+
+from . import slave
+
+class DummyProject(object):
+    def __init__(self):
+        self.BUILDERS = []
+
+def try_import_project(project, scope):
+    try:
+        mod = __import__('machines.local.%s' % project, fromlist=['*'], level=1)
+    except ImportError:
+        mod = DummyProject()
+    scope[project] = mod
+
+try_import_project('vtk', globals())
+try_import_project('vtksuperbuild', globals())
+
+try_import_project('catalyst', globals())
+try_import_project('paraview', globals())
+try_import_project('paraviewsuperbuild', globals())
+
+try_import_project('cmb', globals())
+try_import_project('cmbsuperbuild', globals())
+
+BUILDERS = {
+    'VTK': vtk.BUILDERS,
+    'VTKSuperbuild': vtksuperbuild.BUILDERS,
+
+    'Catalyst': catalyst.BUILDERS,
+    'ParaView': paraview.BUILDERS,
+    'ParaViewSuperbuild': paraviewsuperbuild.BUILDERS,
+
+    'CMB': cmb.BUILDERS,
+    'CMBSuperbuild': cmbsuperbuild.BUILDERS,
+}
+
+def get_buildslave():
+    return slave.SLAVE
+
+def get_builders(project):
+    return BUILDERS.get(project, [])
