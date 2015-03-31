1582b6df658ed3ec1f672f94012fbf2341efffc0
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-31 17:41:00 -0400

    osx: don't use make -i

--- projects/common/superbuild.py
+++ projects/common/superbuild.py
@@ -24,10 +24,5 @@ os = projects.merge_config(options.os, {
         'upload_file_patterns:project': [
             '*.dmg',
         ],
-
-        'configure_options:project': {
-            # CMake is picking make -i as default, which ends up ignoring errors and wasting time!
-            'MAKE_COMMAND:STRING': '/usr/bin/make',
-        },
     },
 })
