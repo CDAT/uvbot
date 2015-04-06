dc6fd3da72c79bdead92e532bb54113584577c1c
Ben Boeckel <ben.boeckel@kitware.com>
2015-04-06 11:41:53 -0400

    endor, talosiv: load the developer cache file

--- machines/endor/cmb.py
+++ machines/endor/cmb.py
@@ -7,9 +7,7 @@ __all__ = [
 ]
 
 defprops = {
-    'configure_options:builderconfig': {
-        #'ctest_local_extra_options_file:FILEPATH': '/home/kitware/dashboards/projects/cmb/developer/cmb-Developer-Config.cmake',
-    },
+    'configure_initial_cache': '/home/kitware/dashboards/data/cmb/developer/cmb-Developer-Config.cmake',
 }
 
 buildsets = [
