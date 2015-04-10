1654ac6481c9b6ba038872cde0373019e63e87e1
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-04-10 07:55:58 -0400

    Always load secrets.json

--- machines/secrets.py
+++ machines/secrets.py
@@ -5,16 +5,6 @@ __all__ = [
     'SECRETS',
 ]
 
-if 'KW_BUILDBOT_PRODUCTION' in os.environ:
-    json_path = os.path.join(os.path.dirname(__file__), 'secrets.json')
-    with open(json_path, 'r') as fin:
-        SECRETS = json.load(fin)
-else:
-    class DummySecrets(object):
-        def __init__(self, types):
-            self.types = types
-        def __getitem__(self, name):
-            if len(self.types) == 1:
-                return self.types[0]()
-            return DummySecrets(self.types[1:])
-    SECRETS = DummySecrets((DummySecrets, str,))
+json_path = os.path.join(os.path.dirname(__file__), 'secrets.json')
+with open(json_path, 'r') as fin:
+    SECRETS = json.load(fin)
