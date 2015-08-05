45a0b31e262ff30dd0f38d8030bb7c5e273fd692
Charles Doutriaux <doutriaux1@llnl.gov>
2015-08-05 16:57:17 -0700

    tweaked sam fake github push script to hide secret

---
+++ fake_github_push_event_request.py
@@ -0,0 +1,21 @@
+import requests
+
+
+url = "http://aims4.llnl.gov:9981/proxy"
+
+
+with open("fake_github_push_event_request.json") as f:
+	contents = f.read()
+
+import hmac
+import hashlib
+
+secret = raw_input("Enter your webhook secret")
+h = hmac.new(secret, contents, hashlib.sha1)
+
+response = requests.post(url, data=contents, headers={
+    "Content-type": "application/json",
+    "X-Hub-Signature": "sha1=%s" % h.hexdigest(),
+    "X-Github-Event": "push",
+    "X-Github-Delivery": "1b1f7f00-3bb0-11e5-8759-a9b99ff1d684"
+})
