c85bceb44c5777f96a609cfe15bcfd7ad19a90fb
Charles Doutriaux <doutriaux1@llnl.gov>
2015-10-12 14:11:00 -0700

    need to checkout a bit more history on baseline for merge purposes

--- slave/slave.py
+++ slave/slave.py
@@ -114,7 +114,12 @@ def process_commit(project,obj):
    if commit["message"].find("##bot##no-merge-master")==-1:
      previous = cmd
      os.chdir(testdata_dir)
-     # for pixture we want ff or it thinks conflicts everywhwrre
+     # CMake does not checkout the whole history, this can lead to conflict
+     # with merge command bellow
+     cmd = "git fetch --depth=10"
+     if process_command(project,commit,cmd,previous,testdata_dir)!=0: return
+     previous = cmd
+     # for pictures we want ff or it thinks conflicts everywhwrre
      cmd = "git merge master --no-commit"
      if process_command(project,commit,cmd,previous,testdata_dir)!=0: return
    # run ctest
