a2b7a9a1f1a419154706517d514e58e05fcd2919
Charles Doutriaux <doutriaux1@llnl.gov>
2016-01-21 14:15:52 -0800

    kill does not care about cwd

--- slave/slave.py
+++ slave/slave.py
@@ -37,7 +37,7 @@ def process_commit(project,obj):
 
    if os.uname()[0] == "Darwin":
       cmd = os.path.join(project["working_directory"],"kill_python_died_window.applescript")
-      p = subprocess.Popen(shlex.split(cmd),stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd=cwd)
+      p = subprocess.Popen(shlex.split(cmd),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
       out,err = p.communicate()
    cmd = None
    # First step go to working directory
